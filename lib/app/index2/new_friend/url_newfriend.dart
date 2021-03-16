dd1a78ff0804fb0490f47de293022c3bfc8f4d2c
Tuuz <silverhawk@foxmail.com>
2021-03-16 20:29:38 +0800

    TuuzPC

---
+++ lib/app/index2/new_friend/url_newfriend.dart
@@ -0,0 +1,14 @@
+import 'package:tuuzim_flutter/config/url.dart';
+
+class Url_Newfriend extends Url {
+  static String Request_list = "/v1/request/request/list";
+
+  /*
+
+参数名	参数类型	必传	缺省值	描述
+id	i	Y
+approve	bool	Y
+comment	str	Y
+   */
+  static String Operation = "/v1/request/request/operation";
+}
